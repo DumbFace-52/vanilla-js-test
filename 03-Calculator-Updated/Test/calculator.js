7a6eea704a5d51b6fe8648c48aaf7064422125ad
knight <tinnoowuor@gmail.com>
2024-11-13 10:18:52 +0300

    Submit Updated Digital Calculator

---
+++ 03-Calculator-Updated/Test/calculator.js
@@ -0,0 +1,9 @@
+// Using Functions to avoid reuse of code;
+
+// initialize a Variable calculation to an empty string;
+let calculation = "";
+
+function updateCalculator(value) {
+    calculation += value;
+    console.log(calculation);
+}
